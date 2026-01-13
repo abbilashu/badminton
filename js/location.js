b998fbadaf8420ec61de6eedab539553f0654ccf
badmintonleague <abbilash.u@gmail.com>
2026-01-12 19:22:24 -0500

    Create location.js

---
+++ js/location.js
@@ -0,0 +1,11 @@
+function toggleLocationMenu() {
+  document
+    .getElementById("locationOptions")
+    .classList.toggle("hidden");
+}
+
+function switchLocation(city) {
+  if (city === "pickering") {
+    window.location.href = "https://badmintonleague.github.io/pickering/leaderboard.html";
+  }
+}
